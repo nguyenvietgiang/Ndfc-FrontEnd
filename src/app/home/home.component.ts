9a3400e741ce1c180f9bd959526e8d81fe255784
Giang <nguyenvietgiang1110@gmail.com>
2023-06-05 09:58:01 +0700

    Hiện Silder Từ API NDFC-Backend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HomesvService } from '../Services/homesv.service';
+//sử lý các tệp tin lấy từ localhost
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  slideData: any;
+  constructor(private homeService: HomesvService, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    this.getSlideData();
   }
 
+  getSlideData(): void {
+    this.homeService.getSlideData().subscribe(
+      (data) => {
+        this.slideData = data.content;
+        // Xử lý URL để tạo URL an toàn
+        this.slideData.forEach((item: any) => {
+          item.safeImg = this.sanitizer.bypassSecurityTrustResourceUrl(item.img);
+        });
+      },
+      (error) => {
+        console.error('Lỗi khi gọi API', error);
+      }
+    );
+  }
 }
