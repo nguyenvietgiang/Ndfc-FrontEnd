c915a8961fbcdd9c8eb90b989b3a39c1b87ba6cd
Giang <nguyenvietgiang1110@gmail.com>
2023-06-06 10:26:47 +0700

    Sửa UI Home và ghép API xem thông tin player

--- src/app/Services/lineupsv.service.ts
+++ src/app/Services/lineupsv.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LineupsvService {
+  private apiUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getPlayerData(): Observable<any> {
+    const url = `${this.apiUrl}/Player`;
+    return this.http.get(url);
+}
 }
--- src/app/lineup/lineup.component.ts
+++ src/app/lineup/lineup.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LineupsvService } from '../Services/lineupsv.service';
+//sử lý các tệp tin lấy từ localhost
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-lineup',
   templateUrl: './lineup.component.html',
   styleUrls: ['./lineup.component.css']
 })
 export class LineupComponent implements OnInit {
-
-  constructor() { }
+  playerData: any;
+  constructor(private lineupService: LineupsvService, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    this.getLineupData();
   }
-
+  
+  getLineupData(): void {
+    this.lineupService.getPlayerData().subscribe(
+      (data) => {
+        this.playerData = data.content;
+        // Xử lý URL để tạo URL an toàn
+        this.playerData.forEach((item: any) => {
+          item.safeImg = this.sanitizer.bypassSecurityTrustResourceUrl(item.sImg);
+        });
+      },
+      (error) => {
+        console.error('Lỗi khi gọi API', error);
+      }
+    );
+  }
 }
